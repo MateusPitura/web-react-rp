c6b962f622f1a5cc0ff0b81e688174220b8d6010
MateusPitura <mateuspitura@gmail.com>
2025-04-30 22:27:55 -0300

    feat: added material icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='bg-light-primary-container'>Teste</div>
+    <div className='bg-light-primary-container'>
+      <ThreeDRotation/>
+    </div>
   </StrictMode>,
 )
