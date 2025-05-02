e9173432a3baf8b25e6efe05382701a54e6fb3b5
MateusPitura <mateuspitura@gmail.com>
2025-05-01 21:16:54 -0300

    feat: added profile picture in rating

--- src/domains/rating/components/Card.tsx
+++ src/domains/rating/components/Card.tsx
@@ -5,17 +5,22 @@ interface CardProperties {
   description: string;
   title: string;
   date: string;
+  image: string;
 }
 
 export default function Card({
   date,
   description,
   title,
+  image,
 }: CardProperties): ReactNode {
   return (
     <div className="w-full flex justify-center">
       <div className="bg-light-surface p-4 shadow-md rounded-md text-light-on-surface flex flex-col gap-2 m-2 min-w-72 max-w-72 min-h-48 max-h-48">
-        <p className="text-title-large">{title}</p>
+        <div className="flex gap-2 items-center">
+          <img src={image} width={32} height={32} className="rounded-full" />
+          <p className="text-title-large">{title}</p>
+        </div>
         <div className="flex gap-1 justify-between items-center">
           <div className="flex gap-1 text-light-primary text-label-large">
             <Icon iconName="Star" />
--- src/domains/rating/components/Rating.tsx
+++ src/domains/rating/components/Rating.tsx
@@ -5,6 +5,11 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { sections } from "../../global/constants/sections";
+import Person1 from "../../../assets/person1.webp";
+import Person2 from "../../../assets/person2.webp";
+import Person3 from "../../../assets/person3.webp";
+import Person4 from "../../../assets/person4.webp";
+import Person5 from "../../../assets/person5.webp";
 
 const settings = {
   infinite: true,
@@ -32,26 +37,31 @@ export default function Rating(): ReactNode {
             title="Heraclio Barbosa Jr"
             date="6 de abril de 2025"
             description="Me senti muito bem na consulta e adorei as explicações detalhadas"
+            image={Person1}
           />
           <Card
             title="T.P"
             date="9 de abril de 2025"
             description="Atendimento foi ótimo e pontual, educado e atencioso"
+            image={Person2}
           />
           <Card
             title="W.R"
             date="3 de abril de 2025"
             description="Suas orientações me ajudaram a colocar a vida em ordem."
+            image={Person3}
           />
           <Card
             title="William"
             date="26 de março de 2025"
             description="Estou satisfeito com o atendimento. Acredito muito na recuperação"
+            image={Person4}
           />
           <Card
             title="junior Stival"
             date="20 de março de 2025"
             description="Sem palavras para descrever o atendimento. Muito obrigado pela atenção Dr"
+            image={Person5}
           />
         </Slider>
       </div>
